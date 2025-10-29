61cffe4a5ccdd3055bac86cafcecc290cddbc0b9
Elsa-1995 <ebyhb@mail.ru>
2025-10-29 19:39:00 +0300

    Delete 01_lesson directory

--- 01_lesson/lesson_1_task_1.py
+++
@@ -1,3 +0,0 @@
-my_name = "Эльза"
-
-print(my_name)
--- 01_lesson/lesson_1_task_3.py
+++
@@ -1,6 +0,0 @@
-print('Ваше имя:')
-first_name = input()
-print('Ваша фамилия')
-last_name = input()
-
-print('Вас зовут:' + last_name + first_name)
